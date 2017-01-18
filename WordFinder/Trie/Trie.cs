b00ebc27588594f2b7ff67e836b75b2ae66390b3
MrShoes <ianhughes81@googlemail.com>
2017-01-18 09:28:36 +0000

    Added some of the Trie code.

---
+++ WordFinder/Trie/Trie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFinder.Trie
+{
+    class Trie
+    {
+        /// <summary>
+        /// Gets or sets the nodes.
+        /// </summary>
+        /// <value>
+        /// The nodes.
+        /// </value>
+        public IDictionary<char, TrieNode> Nodes { get; set; }
+
+        public Trie()
+        {
+            Nodes = new Dictionary<char, TrieNode>();
+        }
+
+        protected TrieNode Prefix(string s)
+        {
+            TrieNode node;
+            if(!Nodes.ContainsKey(s[0]))
+            {
+                node = new TrieNode(s[0], 0, null);
+
+            }
+        }
+    }
+}
---
+++ WordFinder/Trie/TrieNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordFinder.Trie
+{
+    class TrieNode
+    {
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public char Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent node.
+        /// </summary>
+        /// <value>
+        /// The parent node.
+        /// </value>
+        public TrieNode Parent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the children.
+        /// </summary>
+        /// <value>
+        /// The children.
+        /// </value>
+        public IDictionary<char,TrieNode> Children { get; set; }
+
+        /// <summary>
+        /// Gets or sets the depth.
+        /// </summary>
+        /// <value>
+        /// The depth.
+        /// </value>
+        public int Depth { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is a leaf.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is a leaf; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsLeaf { get { return Children.Count == 0; } }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrieNode"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="depth">The depth.</param>
+        /// <param name="parent">The parent.</param>
+        public TrieNode(char value, int depth, TrieNode parent)
+        {
+            Value = value;
+            Depth = depth;
+            Parent = parent;
+            Children = new Dictionary<char, TrieNode>();
+        }
+
+        /// <summary>
+        /// Finds the child.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public TrieNode FindChild(char value)
+        {
+            if (Children.ContainsKey(value))
+                return Children[value];
+            else
+                return null;
+        }
+
+    }
+}
